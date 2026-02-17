9470e2890f2aaf114a63495399fec956c78ee961
Yueting Liu <yueting1106.liu@gmail.com>
2026-02-17 16:31:58 +0100

    set transparent background in nvim

---
+++ dotfiles/config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  "tokyonight.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
