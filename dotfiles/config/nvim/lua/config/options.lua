72b85fa53e2e63993fa4592c7d187e43b40479b3
Yueting Liu <yueting1106.liu@gmail.com>
2026-03-09 20:26:02 +0100

    UI: disable animation
    Animation slows down UI

--- dotfiles/config/nvim/lua/config/options.lua
+++ dotfiles/config/nvim/lua/config/options.lua
@@ -4,23 +4,25 @@
 
 -- -- Use system clipboard (works well on Fedora GNOME)
 -- vim.opt.clipboard = "unnamedplus"
--- 
+--
 -- -- Make UI a bit calmer / informative
 -- vim.opt.number = true
 -- vim.opt.relativenumber = true
 -- vim.opt.signcolumn = "yes"
 -- vim.opt.cursorline = true
--- 
+--
 -- -- Better searching
 -- vim.opt.ignorecase = true
 -- vim.opt.smartcase = true
--- 
+--
 -- -- Indentation (adjust if you prefer 4 spaces)
 -- vim.opt.expandtab = true
 -- vim.opt.shiftwidth = 2
 -- vim.opt.tabstop = 2
--- 
+--
 -- -- Faster feel
 -- vim.opt.updatetime = 200
 -- vim.opt.timeoutlen = 400
 
+-- animations are fun, but they can be distracting and slow down the editor. This option disables them.
+vim.g.snacks_animate = false
