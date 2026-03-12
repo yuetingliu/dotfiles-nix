09f2b409a384abfbdddefccce4dfd34d9230148e
Yueting Liu <yueting1106.liu@gmail.com>
2026-03-12 23:03:18 +0100

    Run keychain only once

--- modules/shell.nix
+++ modules/shell.nix
@@ -29,7 +29,9 @@
       alias cat="bat"
 
       if command -q keychain
-        eval (keychain --quiet --eval --nogui --ignore-missing -Q id_ed25519 id_rsa)
+        if not set -q SSH_AUTH_SOCK; or not test -S "$SSH_AUTH_SOCK"
+          eval (keychain --quiet --eval --nogui --ignore-missing -Q id_ed25519 id_rsa)
+        end
       end
     '';
   };
