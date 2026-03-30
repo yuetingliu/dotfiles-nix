30fd4c3b9407224440e03e3f384ded263245fc68
Yueting Liu <yueting1106.liu@gmail.com>
2026-03-30 21:33:09 +0200

    bring copilot back
    The copilot package now uses copilot-lsp and is very responsive

--- dotfiles/config/doom/config.el
+++ dotfiles/config/doom/config.el
@@ -715,28 +715,28 @@
   )
 )
 
-;; ;; programming
-;; ;; -----------
-;; ;; -----------
-;; ;; set up github copilot
-;; ;; accept completion from copilot and fallback to company
-;; (use-package! copilot
-;;   :hook (prog-mode . copilot-mode)
-;;   :bind (:map copilot-completion-map
-;;               ("<tab>" . 'copilot-accept-completion)
-;;               ("TAB" . 'copilot-accept-completion)
-;;               ("C-TAB" . 'copilot-accept-completion-by-word)
-;;               ("C-<tab>" . 'copilot-accept-completion-by-word)
-;;               ("M-n" . copilot-next-completion)
-;;               ("M-p" . copilot-previous-completion)
-;;         )
-;;   :config
-;;     (add-to-list 'copilot-indentation-alist '(prog-mode 4))
-;;     (add-to-list 'copilot-indentation-alist '(org-mode 2))
-;;     (add-to-list 'copilot-indentation-alist '(text-mode 2))
-;;     (add-to-list 'copilot-indentation-alist '(closure-mode 2))
-;;     (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2))
-;; )
+;; programming
+;; -----------
+;; -----------
+;; set up github copilot
+;; accept completion from copilot and fallback to company
+(use-package! copilot
+  :hook (prog-mode . copilot-mode)
+  :bind (:map copilot-completion-map
+              ("<tab>" . 'copilot-accept-completion)
+              ("TAB" . 'copilot-accept-completion)
+              ("C-TAB" . 'copilot-accept-completion-by-word)
+              ("C-<tab>" . 'copilot-accept-completion-by-word)
+              ("M-n" . copilot-next-completion)
+              ("M-p" . copilot-previous-completion)
+        )
+  :config
+    (add-to-list 'copilot-indentation-alist '(prog-mode 4))
+    (add-to-list 'copilot-indentation-alist '(org-mode 2))
+    (add-to-list 'copilot-indentation-alist '(text-mode 2))
+    (add-to-list 'copilot-indentation-alist '(closure-mode 2))
+    (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2))
+)
 
 
 ;; LSP related
