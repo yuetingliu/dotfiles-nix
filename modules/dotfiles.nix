3bce2b82f4f7fd3527b539e74b8825b7a26b6432
Yueting Liu <yueting1106.liu@gmail.com>
2026-06-27 12:56:47 +0200

    Set kitty to work with fish cross-platform
    By loading a local conf, generated by home-manager. The local conf sets
    up abs fish path. In this way, both linux and MacOS are well supported

--- modules/dotfiles.nix
+++ modules/dotfiles.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 
 let
-  oos  = config.lib.file.mkOutOfStoreSymlink;
+  oos = config.lib.file.mkOutOfStoreSymlink;
 in
 {
   options.dotfiles.repoPath = lib.mkOption {
@@ -21,7 +21,11 @@ in
     # Doom user config directory (init.el/config.el/packages.el)
     xdg.configFile."doom".source = oos "${config.dotfiles.repoPath}/dotfiles/config/doom";
 
-    # Kitty terminal config directory
-    xdg.configFile."kitty".source = oos "${config.dotfiles.repoPath}/dotfiles/config/kitty";
+    # Kitty terminal config. Keep the main config editable, and generate only
+    # the platform-specific shell path.
+    xdg.configFile."kitty/kitty.conf".source = oos "${config.dotfiles.repoPath}/dotfiles/config/kitty/kitty.conf";
+    xdg.configFile."kitty/local.conf".text = ''
+      shell ${config.home.profileDirectory}/bin/fish
+    '';
   };
 }
