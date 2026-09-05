3f6134b78fb551b5444e000db3ecaa5b5163c81e
Yueting Liu <yueting1106.liu@gmail.com>
2026-09-05 10:26:16 +0200

    CLEAN: remove unused config

--- dotfiles/config/doom/init.el
+++ dotfiles/config/doom/init.el
@@ -166,11 +166,11 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
-       (python            ; beautiful is better than ugly
-        +lsp
-        +pyright
-        +uv
-        +ruff)
+       python            ; beautiful is better than ugly
+       ; +lsp
+       ; +pyright
+       ; +uv
+       ; +ruff)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
